b7fd7fc1e4874082ebcbbb67d9bf391ad8ce0567
bikoyjeanne-afk <bikoyjeanne@gmail.com>
2025-12-23 13:31:21 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = "sos-femmes-cm-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
